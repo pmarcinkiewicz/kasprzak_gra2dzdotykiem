31e4f676e9a916013ac650de709f517a80dfa9a4
Piotr Marcinkiewicz <pmarcinkiewicz@darkspacelab.com>
2017-05-25 12:30:09 +0200

    Initial commit

---
+++ app/src/main/java/com/darkspacelab/gra2dzdotykiem/GlownaActivity.java
@@ -0,0 +1,13 @@
+package com.darkspacelab.gra2dzdotykiem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlownaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glowna);
+    }
+}
