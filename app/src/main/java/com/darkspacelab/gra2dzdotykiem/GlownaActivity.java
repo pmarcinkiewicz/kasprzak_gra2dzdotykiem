c102334f2c64967159b75b839138b505f9c0b454
Piotr Marcinkiewicz <pmarcinkiewicz@darkspacelab.com>
2017-05-25 12:48:53 +0200

    Główna Activity na pełny ekran i bez paska tytułowego

--- app/src/main/java/com/darkspacelab/gra2dzdotykiem/GlownaActivity.java
+++ app/src/main/java/com/darkspacelab/gra2dzdotykiem/GlownaActivity.java
@@ -2,12 +2,21 @@ package com.darkspacelab.gra2dzdotykiem;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GlownaActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Ustaw na cały ekran
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Wyłącz pasek tytułowy
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_glowna);
     }
 }
